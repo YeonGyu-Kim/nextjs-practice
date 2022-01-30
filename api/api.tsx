172e7856e6b2a2707dc33f0fc8fc13da68df66a7
YeonGyu-Kim <62788631+YeonGyu-Kim@users.noreply.github.com>
2022-01-30 23:03:35 +0900

    fix: 브라우저 네트워크에서 api key 숨겨지도록 수정
    env 파일에 api key 추가
    config 파일에 redirects, rewrites 추가

--- api/api.tsx
+++ api/api.tsx
@@ -1,16 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
-  params: {
-    api_key: "6d095688f630b2115d0be560ed267853",
-    language: "ko",
-    region: "kr",
-    page: 1,
-  },
-});
-
 export const movieApi = {
   popular: async () =>
-    await api.get("movie/popular").then((response) => response.data),
+    await axios.get("api/moviePopular").then((response) => response.data),
 };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/:slug",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/moviePopular",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko&region=kr&page=1`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
